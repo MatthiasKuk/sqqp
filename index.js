08f1d00a492986975ba4143339b403f0b444cdfe
Matthias Kuklinski <matthias.kuk@gmail.com>
2018-06-23 14:48:58 +0200

    Added express.js support

--- index.js
+++ index.js
@@ -9,6 +9,14 @@
  * @return {object}
  */
 
+function splitMultiple(queryUnitProps, queryUnitParam, delimiter) {
+  const operator = queryUnitParam[1][0];
+  const filterValues = queryUnitParam[1][1];
+  queryUnitProps[operator] = filterValues
+    .substring(1, filterValues.length - 1)
+    .split(delimiter);
+}
+
 module.exports = {
   parse: (query, options) => {
     if (!options) {
@@ -20,25 +28,38 @@ module.exports = {
             delimiter: ',',
             boundaries: [ '[',']' ]
           }
-        }
+        },
+        express: true
       }
     }
-    const queryParams = {};
-    query.split('&')
-      .map(queryUnit => queryUnit.split(options.delimiters.operator))
-      .map(queryUnitParams => queryUnitParams.map(queryUnitParam => queryUnitParam.split(options.delimiters.value)))
-      .forEach(queryUnitParam => {
-        const queryUnitProps = {};
-        if ((queryUnitParam[1][1][0] === options.delimiters.queryParams.boundaries[0]) &&
-          (queryUnitParam[1][1][queryUnitParam[1][1].length - 1] === options.delimiters.queryParams.boundaries[1])) {
-            queryUnitProps[queryUnitParam[1][0]] = queryUnitParam[1][1]
-              .substring(1, queryUnitParam[1][1].length - 1)
-              .split(options.delimiters.queryParams.delimiter);
-          } else {
-            queryUnitProps[queryUnitParam[1][0]] = queryUnitParam[1][1];
-          }
-          queryParams[queryUnitParam[0]] = queryUnitProps;
-      })
-  return queryParams;
+
+    const parsedQuery = {};
+    if (!options.express) {
+      query.split('&')
+        .map(queryUnit => queryUnit.split(options.delimiters.operator))
+        .map(queryUnitParams => queryUnitParams.map(queryUnitParam => queryUnitParam.split(options.delimiters.value)))
+        .forEach(queryUnitParam => {
+          const parsedQueryUnit = {};
+          const filterValues = queryUnitParam[1][1];
+          if ((filterValues[0] === options.delimiters.queryParams.boundaries[0]) &&
+            (filterValues[filterValues.length - 1] === options.delimiters.queryParams.boundaries[1])) {
+              splitMultiple(parsedQueryUnit, queryUnitParam, options.delimiters.queryParams.delimiter);
+            } else {
+              parsedQueryUnit[queryUnitParam[1][0]] = queryUnitParam[1][1];
+            }
+            parsedQuery[queryUnitParam[0]] = parsedQueryUnit;
+        })
+    } else {
+      for (const queryUnitParam in query) {
+        const parsedQueryUnit = {};
+        const operator = query[queryUnitParam];
+        parsedQueryUnit[queryUnitParam.split(options.delimiters.operator)[1]] = query[queryUnitParam];
+        parsedQuery[queryUnitParam.split(options.delimiters.operator)[0]] = parsedQueryUnit;
+      }
+    }
+  return parsedQuery;
   }
 };
+
+// { 'id:or': '[1,2]', 'name:eq': 'Tim' }
+// { id: { or: [1,2] }, name: { eq: 'Tim' } }
