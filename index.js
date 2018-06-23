3e52f8c1c2523528596781ea9aee9674011fe2d1
Matthias Kuklinski <matthias.kuk@gmail.com>
2018-06-23 13:14:11 +0200

    Added options parameter to allow customisation of the query

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
 /**
- * @desc Parses the provided parameters into objects.
+ * @desc 
+ * Parses the provided parameters into objects, which correspond to the Sequelize filtering-guidelines ('where').
  * @param {string} query
- * @param {string} options
+ * Represents the query.
+ * @param {object} options
+ * Represents an object which allows for customization of the query.
+ * If no options object is provided, a default options object is created and used.
  * @return {object}
  */
 
-// { id: { eq: 1} }
-
 module.exports = {
   parse: (query, options) => {
+    if (!options) {
+      options = {
+        delimiters: {
+          operator: ':',
+          value: '=',
+          queryParams: {
+            delimiter: ',',
+            boundaries: [ '[',']' ]
+          }
+        }
+      }
+    }
     const queryParams = {};
     query.split('&')
-      .map(queryUnit => queryUnit.split(':'))
-      .map(queryUnitParams => queryUnitParams.map(queryUnitParam => queryUnitParam.split('=')))
+      .map(queryUnit => queryUnit.split(options.delimiters.operator))
+      .map(queryUnitParams => queryUnitParams.map(queryUnitParam => queryUnitParam.split(options.delimiters.value)))
       .forEach(queryUnitParam => {
-        const p = {};
-        if ((queryUnitParam[1][1][0] === '[') && (queryUnitParam[1][1][queryUnitParam[1][1].length - 1] === ']')) {
-          p[queryUnitParam[1][0]] = queryUnitParam[1][1]
-            .substring(1, queryUnitParam[1][1].length - 1)
-            .split(',');
-        } else {
-          p[queryUnitParam[1][0]] = queryUnitParam[1][1];
-        }
-        // p[queryUnitParam[1][0]] = queryUnitParam[1][1];
-        queryParams[queryUnitParam[0]] = p;
+        const queryUnitProps = {};
+        if ((queryUnitParam[1][1][0] === options.delimiters.queryParams.boundaries[0]) &&
+          (queryUnitParam[1][1][queryUnitParam[1][1].length - 1] === options.delimiters.queryParams.boundaries[1])) {
+            queryUnitProps[queryUnitParam[1][0]] = queryUnitParam[1][1]
+              .substring(1, queryUnitParam[1][1].length - 1)
+              .split(options.delimiters.queryParams.delimiter);
+          } else {
+            queryUnitProps[queryUnitParam[1][0]] = queryUnitParam[1][1];
+          }
+          queryParams[queryUnitParam[0]] = queryUnitProps;
       })
-  return queryParams;  
+  return queryParams;
   }
 };
